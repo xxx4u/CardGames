f7ea5fd8149bc2b3f1023ec7f7c6073936faae56
otasyn <otasyn@gmail.com>
2013-11-04 17:14:54 -0500

    Friends List and Games List now populate as rows on the view.

--- src/android/otasyn/cardgames/manage/account/activity/FriendsListActivity.java
+++ src/android/otasyn/cardgames/manage/account/activity/FriendsListActivity.java
@@ -6,7 +6,6 @@
 package android.otasyn.cardgames.manage.account.activity;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,7 +14,11 @@ import android.otasyn.cardgames.manage.account.asynctask.CurrentUserTask;
 import android.otasyn.cardgames.manage.account.asynctask.FriendsListTask;
 import android.otasyn.cardgames.manage.account.dto.Friend;
 import android.otasyn.cardgames.manage.account.dto.SimpleUser;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FriendsListActivity extends Activity {
@@ -34,34 +37,78 @@ public class FriendsListActivity extends Activity {
         }
     }
 
+    final List<Friend> friends = new ArrayList<>();
+
+    private LinearLayout friendsListLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
 
+        friendsListLayout = (LinearLayout) findViewById(R.id.friendsListLayout);
+
         friendsList();
     }
 
     private void friendsList() {
-        List<Friend> friends = null;
+        friends.clear();
         try {
-            friends = new FriendsListTask().execute().get();
+            friends.addAll(new FriendsListTask().execute().get());
         } catch (Exception e) { }
 
-        String friendsString = null;
-        if (friends != null) {
-            friendsString = "Friends[" + friends.size() + "]:";
+        if (!friends.isEmpty()) {
+            friendsListLayout.removeAllViews();
             for (Friend friend : friends) {
-                friendsString += "\n  " + friend.getFirstname() + " " + friend.getLastname();
+                LinearLayout friendLayout = new LinearLayout(this);
+                friendLayout.setOrientation(LinearLayout.VERTICAL);
+                LinearLayout.LayoutParams friendLayoutParams = new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                friendLayoutParams.bottomMargin = getResources().getDimensionPixelSize(R.dimen.listEntryBottomMargin);
+                friendLayout.setLayoutParams(friendLayoutParams);
+
+                // Display name
+                TextView friendName = new TextView(this);
+                friendName.setText(friend.getFirstname() + " " + friend.getLastname());
+                friendName.setTextColor(getResources().getColor(R.color.listEntryHeader));
+                LinearLayout.LayoutParams friendNameLayoutParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                friendNameLayoutParams.leftMargin = getResources().getDimensionPixelSize(R.dimen.listEntryHeaderLeftMargin);
+                friendName.setLayoutParams(friendNameLayoutParams);
+                friendLayout.addView(friendName);
+
+                // Display email
+                TextView friendEmail = new TextView(this);
+                friendEmail.setText(friend.getEmail());
+                friendEmail.setTextColor(getResources().getColor(R.color.listEntryDetails));
+                LinearLayout.LayoutParams friendEmailLayoutParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                friendEmailLayoutParams.leftMargin = getResources().getDimensionPixelSize(R.dimen.listEntryDetailsLeftMargin);
+                friendEmail.setLayoutParams(friendEmailLayoutParams);
+                friendLayout.addView(friendEmail);
+
+                // If not confirmed, display "Awaiting Confirmation".
+                if (!friend.isConfirmed()) {
+                    TextView friendConfirmed = new TextView(this);
+                    friendConfirmed.setText("Awaiting Confirmation");
+                    friendConfirmed.setTextColor(getResources().getColor(R.color.listEntryDetails));
+                    LinearLayout.LayoutParams friendConfirmedLayoutParams = new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    friendConfirmedLayoutParams.leftMargin = getResources().getDimensionPixelSize(R.dimen.listEntryDetailsLeftMargin);
+                    friendConfirmed.setLayoutParams(friendConfirmedLayoutParams);
+                    friendConfirmed.setBackgroundDrawable(getResources().getDrawable(R.drawable.awaiting_response_background));
+                    friendConfirmed.setPadding(
+                            getResources().getDimensionPixelSize(R.dimen.awaitingConfirmationLeftRightPadding),
+                            getResources().getDimensionPixelSize(R.dimen.awaitingConfirmationTopBottomPadding),
+                            getResources().getDimensionPixelSize(R.dimen.awaitingConfirmationLeftRightPadding),
+                            getResources().getDimensionPixelSize(R.dimen.awaitingConfirmationTopBottomPadding)
+                    );
+                    friendLayout.addView(friendConfirmed);
+                }
+
+                friendsListLayout.addView(friendLayout);
             }
         }
-
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(FriendsListActivity.this);
-        alertBuilder
-                .setMessage(friendsString != null ? friendsString : "No friends.")
-                .setCancelable(true)
-                .create()
-                .show();
     }
 
 }
--- src/android/otasyn/cardgames/manage/account/activity/GamesListActivity.java
+++ src/android/otasyn/cardgames/manage/account/activity/GamesListActivity.java
@@ -6,7 +6,6 @@
 package android.otasyn.cardgames.manage.account.activity;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,9 +13,13 @@ import android.otasyn.cardgames.R;
 import android.otasyn.cardgames.manage.account.asynctask.CurrentUserTask;
 import android.otasyn.cardgames.manage.account.asynctask.GamesListTask;
 import android.otasyn.cardgames.manage.account.dto.Game;
+import android.otasyn.cardgames.manage.account.dto.GamePlayer;
 import android.otasyn.cardgames.manage.account.dto.SimpleUser;
-import android.otasyn.cardgames.utility.DateUtility;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GamesListActivity extends Activity {
@@ -35,21 +38,63 @@ public class GamesListActivity extends Activity {
         }
     }
 
+    final List<Game> games = new ArrayList<>();
+
+    private LinearLayout gamesListLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games_list);
 
+        gamesListLayout = (LinearLayout) findViewById(R.id.gamesListLayout);
+
         gamesList();
     }
 
     private void gamesList() {
-        List<Game> games = null;
+        games.clear();
         try {
-            games = new GamesListTask().execute().get();
+            games.addAll(new GamesListTask().execute().get());
         } catch (Exception e) { }
 
-        String gamesString = null;
+        if (!games.isEmpty()) {
+            gamesListLayout.removeAllViews();
+            for (Game game : games) {
+                LinearLayout gameLayout = new LinearLayout(this);
+                gameLayout.setOrientation(LinearLayout.VERTICAL);
+                LinearLayout.LayoutParams gameLayoutParams = new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                gameLayoutParams.bottomMargin = getResources().getDimensionPixelSize(R.dimen.listEntryBottomMargin);
+                gameLayout.setLayoutParams(gameLayoutParams);
+
+                // Display type
+                TextView gameType = new TextView(this);
+                gameType.setText(game.getGameType().getName());
+                gameType.setTextColor(getResources().getColor(R.color.listEntryHeader));
+                LinearLayout.LayoutParams gameTypeLayoutParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                gameTypeLayoutParams.leftMargin = getResources().getDimensionPixelSize(R.dimen.listEntryHeaderLeftMargin);
+                gameType.setLayoutParams(gameTypeLayoutParams);
+                gameLayout.addView(gameType);
+
+                // Display players
+                for (GamePlayer gamePlayer : game.getPlayers()) {
+                    TextView gamePlayerName = new TextView(this);
+                    gamePlayerName.setText(gamePlayer.getFirstname() + " " + gamePlayer.getLastname());
+                    gamePlayerName.setTextColor(getResources().getColor(R.color.listEntryDetails));
+                    LinearLayout.LayoutParams gamePlayerLayoutParams = new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    gamePlayerLayoutParams.leftMargin = getResources().getDimensionPixelSize(R.dimen.listEntryDetailsLeftMargin);
+                    gamePlayerName.setLayoutParams(gamePlayerLayoutParams);
+                    gameLayout.addView(gamePlayerName);
+                }
+
+                gamesListLayout.addView(gameLayout);
+            }
+        }
+
+/*        String gamesString = null;
         if (games != null) {
             gamesString = "Games[" + games.size() + "]:";
             for (Game game : games) {
@@ -62,7 +107,7 @@ public class GamesListActivity extends Activity {
                 .setMessage(gamesString != null ? gamesString : "No games.")
                 .setCancelable(true)
                 .create()
-                .show();
+                .show();*/
     }
 
 }
